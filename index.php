d7ad672aaf7052f4a4c4c95919ca492405806c64
nhanhas <mpereira132@hotmail.com>
2017-10-12 23:31:53 +0100

    S#12 not in use image

--- index.php
+++ index.php
@@ -1159,7 +1159,31 @@
 		
 	} 
 	 
-	 
+	/*Not in use*/
+	function storeImage(){
+		/*$data = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABl'
+       . 'BMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDr'
+       . 'EX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r'
+       . '8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
+		$data = base64_decode($data);
+
+		$im = imagecreatefromstring($data);
+		if ($im !== false) {
+		header('Content-Type: image/png');
+		imagepng($im);
+		imagedestroy($im);
+		}
+		else {
+		echo 'An error occurred.';
+		}
+
+		$file = '123.jpg';
+			
+			file_put_contents($file, $data);
+		exit(1);
+			*/
+		
+	}
 	 
 	 
 	
